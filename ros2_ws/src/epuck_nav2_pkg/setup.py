86407b284beb7e42efed60fe43710097a50d040a
markus-scl <markus@schloegl.com>
2025-04-01 10:27:02 +0200

    wp no errors when launching nav2 package

--- ros2_ws/src/epuck_nav2_pkg/setup.py
+++ ros2_ws/src/epuck_nav2_pkg/setup.py
@@ -7,12 +7,14 @@ package_name = 'epuck_nav2_pkg'
 setup(
     name=package_name,
     version='0.0.1',
-    packages=[package_name],
+    packages=[],  # Empty since there's no epuck_nav2_pkg/ module directory
     data_files=[
         ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
         (os.path.join('share', package_name, 'launch'), glob('launch/*.py')),
-        (os.path.join('share', package_name, 'config'), glob('config/*.yaml')), 
+        (os.path.join('share', package_name, 'config'), glob('config/*.yaml')),
+        # Install the script to share/epuck_nav2_pkg/scripts/
+        (os.path.join('share', package_name, 'scripts'), ['scripts/epuck_controller_nav2.py']),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
@@ -23,7 +25,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'epuck_controller_nav2 = epuck_nav2_pkg.scripts.epuck_controller_nav2:main',
+            # Optional: 'epuck_controller_nav2 = epuck_nav2_pkg.scripts.epuck_controller_nav2:main',
         ],
     },
 )
