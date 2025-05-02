acc2b00f5c2a534cc096044c0be34c922ea986c7
Stender98 <ches@itu.dk>
2025-05-02 14:55:31 +0200

    new run script. WIP decentralised launch. worlds not setup. only 2 robots work.

--- ros2_ws/src/epuck_nav2_pkg/launch/epuck_nav2_swarm_launch.py
+++ ros2_ws/src/epuck_nav2_pkg/launch/epuck_nav2_swarm_launch.py
@@ -8,7 +8,8 @@ from launch.actions import (
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from ament_index_python.packages import get_package_share_directory
 import os
-from launch.actions import GroupAction
+from launch.actions import GroupAction, DeclareLaunchArgument, OpaqueFunction
+from launch.substitutions import LaunchConfiguration
 import yaml
 import tempfile
 
@@ -22,12 +23,18 @@ def generate_launch_description():
     config_dir = os.path.join(get_package_share_directory('epuck_nav2_pkg'), 'config')
     rviz_config_path = os.path.join(config_dir, 'nav2_rviz_config.rviz')
     config_path = os.path.join(config_dir, f'nav2_params_swarm.yaml')
-    slam_toolbox_config_path = os.path.join(config_dir, 'slam_toolbox_params.yaml')
-    bringup_dir = get_package_share_directory('nav2_bringup')
-    launch_dir = os.path.join(bringup_dir, 'launch')
 
-    # Webots controller path
-    webots_controller = '/usr/local/webots/webots-controller' if os.getenv('USER') == 'markus' else '/snap/webots/27/usr/share/webots/webots-controller'
+    webots_controller_arg = DeclareLaunchArgument(
+        'webots_controller',
+        default_value='',
+        description='Path to the Webots controller executable'
+    )
+
+    robot_count_arg = DeclareLaunchArgument(
+        'robot_count',
+        default_value='2',
+        description='Number of robots to launch'
+    )
 
     # Nav2 launch file path
     nav2_launch_file = os.path.join(get_package_share_directory('nav2_bringup'), 'launch', 'bringup_swarm_launch.py')
@@ -39,11 +46,102 @@ def generate_launch_description():
 
     ld = LaunchDescription()
 
-    # Set number of robots statically for now
-    num_robots = 2
-    namespaces = [f'robot{i}' for i in range(num_robots)]
+    ld.add_action(webots_controller_arg)
+    ld.add_action(robot_count_arg)
+
+    spawn_robots = OpaqueFunction(function=launch_robots)
+    ld.add_action(spawn_robots)
 
     # Load and modify Slam Toolbox parameters
+    start_slam_toolbox = OpaqueFunction(function=launch_slam_toolbox)
+
+    ld.add_action(TimerAction(
+        period=5.0,
+        actions=[
+            LogInfo(msg="Launching SLAM Toolbox..."),
+            start_slam_toolbox
+        ]
+    ))
+
+    ld.add_action(LogInfo(msg=f"Attempting to include Nav2 launch file: {nav2_launch_file}"))
+
+    # Nav2 bringup
+    nav2_bringup = GroupAction([
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(nav2_launch_file),
+            launch_arguments={
+                'params_file': config_path,
+                'use_sim_time': 'True',
+                'autostart': 'True',
+            }.items()
+        )
+    ])
+
+    # Staggered Nav2 startup
+    ld.add_action(TimerAction(
+        period=10.0,
+        actions=[
+            LogInfo(msg=f"Launching Nav2 with config: {config_path}"),
+            nav2_bringup
+        ]
+    ))
+
+    # RViz launch
+    rviz_launch = ExecuteProcess(
+        cmd=['rviz2', '-d', rviz_config_path],
+        output='screen',
+        name='rviz2'
+    )
+
+    ld.add_action(TimerAction(
+        period=8.0,
+        actions=[
+            LogInfo(msg="Launching RViz2..."),
+            rviz_launch
+        ]
+    ))
+
+    return ld
+
+def launch_robots(context, *args, **kwargs):
+    repo_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '../../epuck_nav2_pkg'))
+    controller_path = os.path.join(repo_dir, 'scripts/swarm_nav_controller.py')
+
+    # Get robot count and webots controller path from LaunchConfigurations
+    num_robots = int(LaunchConfiguration('robot_count').perform(context))
+    webots_controller = LaunchConfiguration('webots_controller').perform(context)
+
+    # Generate namespaces
+    namespaces = [f'robot{i}' for i in range(num_robots)]
+
+    actions = []
+
+    # Launch each robot controller
+    for namespace in namespaces:
+        # Create a launch action for each controller
+        controller_launch = ExecuteProcess(
+            cmd=[webots_controller, f'--robot-name={namespace}', controller_path],
+            output='screen',
+            name=f'epuck_controller_{namespace}',
+            shell=True
+        )
+
+        # Add log info and controller launch action
+        actions.append(LogInfo(msg=f"Launching EPuck controller for {namespace}"))
+        actions.append(controller_launch)
+
+    return actions
+
+def launch_slam_toolbox(context, *args, **kwargs):
+    # Get the SLAM Toolbox config path and namespaces
+    config_dir = os.path.join(get_package_share_directory('epuck_nav2_pkg'), 'config')
+    slam_toolbox_config_path = os.path.join(config_dir, 'slam_toolbox_params.yaml')
+    bringup_dir = get_package_share_directory('nav2_bringup')
+    launch_dir = os.path.join(bringup_dir, 'launch')
+    num_robots = int(LaunchConfiguration('robot_count').perform(context))
+    
+    namespaces = [f'robot{i}' for i in range(num_robots)]
+    
     with open(slam_toolbox_config_path, 'r') as file:
         slam_toolbox_config = yaml.safe_load(file)
 
@@ -74,64 +172,4 @@ def generate_launch_description():
                 }.items(),
             )
     
-    ld.add_action(TimerAction(
-        period=5.0,
-        actions=[
-            LogInfo(msg="Launching SLAM Toolbox..."),
-            slam_toolbox_bringup
-        ]
-    ))
-
-    # Launch loop
-    for namespace in namespaces:
-        # EPuck Webots controller
-        controller_launch = ExecuteProcess(
-            cmd=[webots_controller, f'--robot-name={namespace}', controller_path],
-            output='screen',
-            name=f'epuck_controller_{namespace}',
-            shell=True
-        )
-
-        # Add controller
-        ld.add_action(LogInfo(msg=f"Launching EPuck controller for {namespace}"))
-        ld.add_action(controller_launch)
-
-    ld.add_action(LogInfo(msg=f"Attempting to include Nav2 launch file: {nav2_launch_file}"))
-
-    # Nav2 bringup
-    nav2_bringup = GroupAction([
-        IncludeLaunchDescription(
-            PythonLaunchDescriptionSource(nav2_launch_file),
-            launch_arguments={
-                'params_file': config_path,
-                'use_sim_time': 'True',
-                'autostart': 'True',
-            }.items()
-        )
-    ])
-
-    # Staggered Nav2 startup
-    ld.add_action(TimerAction(
-        period=10.0,
-        actions=[
-            LogInfo(msg=f"Launching Nav2 for {num_robots} robots with config: {config_path}"),
-            nav2_bringup
-        ]
-    ))
-
-    # RViz launch
-    rviz_launch = ExecuteProcess(
-        cmd=['rviz2', '-d', rviz_config_path],
-        output='screen',
-        name='rviz2'
-    )
-
-    ld.add_action(TimerAction(
-        period=8.0 + num_robots,
-        actions=[
-            LogInfo(msg="Launching RViz2..."),
-            rviz_launch
-        ]
-    ))
-
-    return ld
+    return [slam_toolbox_bringup]
